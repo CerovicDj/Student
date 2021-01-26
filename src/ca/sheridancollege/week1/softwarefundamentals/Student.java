ffab2b2ddd3a36bc62ded4b69dca4c16abdbf85a
Djordje <cerovic@shernet.sheridancollege.ca>
2021-01-25 21:02:31 -0500

    First Commit - PartTime Branch

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -10,6 +10,27 @@ package ca.sheridancollege.week1.softwarefundamentals;
  * @author serbi -- Jan 25, 2021
  */
 public class Student {
+
+    private String numHours;
+
+    /**
+     * Get the value of numHours
+     *
+     * @return the value of numHours
+     */
+    public String getNumHours() {
+        return numHours;
+    }
+
+    /**
+     * Set the value of numHours
+     *
+     * @param numHours new value of numHours
+     */
+    public void setNumHours(String numHours) {
+        this.numHours = numHours;
+    }
+
     
     private String name;
 
